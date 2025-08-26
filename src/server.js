1e59fe8dbeb814da7f5ab5e2ef1602acf0774db3
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-08-26 22:45:40 +0430

    add new dependencies (cors, helmet, morgan) and configure middleware in server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
 // Middleware
 app.use(express.json());
+
+// Centralized error middleware:
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
 
 // Test Endpoint
 app.get('/api/test', (req, res) => {
